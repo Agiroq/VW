8e5c6a61004ed53ceeb2bc2d5f27e4eebf86793b
Unknown <agiroq@uoc.edu>
2017-02-09 18:57:22 +0100

    heater triger

--- heater.py
+++ heater.py
@@ -1,15 +1,21 @@
 import machine
-
+MIN = 10
 heat_state = machine.Pin(3, machine.Pin.IN)
 heat_out = machine.Pin(4, machine.Pin.OUT)
 heat_trig = machine.Pin(5, machine.Pin.IN)
-heat_time = 600*1000
+heat_time = 60000*MIN
+
 def stop_heater(heat_o):
     heat_o.low()
 
-def start_heater(heat_o):
-    if(heat_out):
-        tim.init(period=heat_time, mode=Timer.ONE_SHOT,
+def start_heater(heat_o, time):
+    tim.init(period=time, mode=Timer.ONE_SHOT,
                 callback=lambda t:stop_heater(heat_out))
 
 tim = machine.Timer(4)
+
+#trigger button define
+heat_trig.irq(trigger=Pin.IRQ_RISING,
+              handler=lambda t:start_heater(heat_out, heat_time))
+heat_state.irq(trigger=Pin.IRQ_RISING,
+              handler=lambda t:start_heater(heat_out, heat_time))
