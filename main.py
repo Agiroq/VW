2be0807266b084543894dfaef8288d729cfd5cec
albert <agiroq@gmail.com>
2017-02-10 18:57:29 +0100

    Added basic heater control

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import machine
 from machine import Timer
 
-GASANALOG = 0
-ALARMLED = 13
+#constants
+MIN_HEAT = 10
 TIMECHECK = 5000
 LLINDAR_GAS = 480
 
+#pins     CODE  BOARD
+GASANALOG = 0   #A0
+GASALARM = 13   #D7
+HEAT_O = 14     #D5
+HEAT_T = 12     #D6
+HEAT_I = 4      #D2
+
 gasA = machine.ADC(GASANALOG)
-gasLED = machine.Pin(ALARMLED, machine.Pin.OUT)
+gasLED = machine.Pin(GASALARM, machine.Pin.OUT)
+
+heat_state = machine.Pin(HEAT_I, machine.Pin.IN)
+heat_out = machine.Pin(HEAT_O, machine.Pin.OUT)
+heat_trig = machine.Pin(HEAT_T, machine.Pin.IN)
+heat_time = 60000*MIN_HEAT
+
+#function definitions
 
 def checkGas(sensor, level):
     value = sensor.read()
@@ -24,5 +38,18 @@ def gasCheck_start(timer, time, sensor, level):
 def gasCheck_stop(timer):
     timer.deinit()
 
+def stop_heater(heat_o):
+    heat_o.low()
+
+def start_heater(timer, heat_o, time):
+    timer.init(period=time, mode=Timer.ONE_SHOT,
+                callback=lambda t:stop_heater(heat_out))
+
+tim = machine.Timer(4)
+
+#trigger button define
+heat_trig.irq(trigger=machine.Pin.IRQ_RISING,
+              handler=lambda t:start_heater(tim, heat_out, heat_time))
+
 t = machine.Timer(-1)
-gasCheck_start(t, TIMECHECK, gasA, LLINDAR_GAS)
+gasCheck_start(tim, TIMECHECK, gasA, LLINDAR_GAS)
